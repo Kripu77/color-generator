203e9646ad5653e8b72a1a26d1f8cbdd4e775fc6
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-14 10:17:15 +1100

    basic styling

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import "./CSS/style.css"
 const App = () => {
     return (
         <main>
             <h1> Color Generator</h1>
             <section>
                 <form>
-                    <label htmlFor='generator'> Color Generator</label>\
+                    <label htmlFor='generator'> Color Generator</label>
                     <input type="text" id="generator" name="generator"></input>
                     <button> Generate</button>
                 </form>
