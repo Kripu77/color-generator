e310e0c4f6a51a820373caf5a013ee6980cb71c7
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-14 10:37:11 +1100

    state value update

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,7 +1,13 @@
 import React,{useState} from 'react'
+import {ToastContainer, toast} from "react-toastify";
+  import 'react-toastify/dist/ReactToastify.css';
+  import Values from 'values.js'
+
 import "./CSS/style.css"
 const App = () => {
     const [value, setValue] = useState('')
+    const[error, setError] = useState(false);
+    const[list, setList] =useState([])
 
     //prevents default behaviour
     const handleSubmit = (e)=>{
