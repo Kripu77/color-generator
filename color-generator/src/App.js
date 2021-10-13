e1ff7198cf77bc140fffc374e700ed47363805c6
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-14 10:22:16 +1100

    functional state update

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./CSS/style.css"
 const App = () => {
+    const [value, setvalue] = useState('')
+
+    //prevents default behaviour
+    const handleSubmit = (e)=>{
+e.preventDefault();
+    }
     return (
         <main>
             <h1> Color <span> Generator</span></h1>
             <section>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label htmlFor='generator'> Color <span> Generator</span></label>
                     <input type="text" id="generator" name="generator"></input>
-                    <button> Generate</button>
+                    <button type='submit'> Generate</button>
                 </form>
             </section>
         </main>
