c87518e4b522840fc3fe41b45da145e1c122ef21
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-18 16:16:09 +1100

    Production complete

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -6,9 +6,9 @@ import Singlecolor from './Components/Singlecolor';
 
 import "./CSS/style.css"
 const App = () => {
-    const [value, setValue] = useState('')
+    const [value, setValue] = useState('#f10525')
     const[error, setError] = useState(false);
-    const[list, setList] =useState([])
+    const[list, setList] =useState(new Values('#f15025').all(10))
     console.log(Values)
 
     //prevents default behaviour
@@ -17,19 +17,20 @@ const App = () => {
 e.preventDefault();
 try{
 let colors = new Values(value).all(10)
-console.log(colors)
-setList(colors)
-console.log(value)}
+// console.log(colors)
+setList(colors)}
+// console.log(value)}
 catch(error){
     setError(true)
     toast.error("The value is not acceptable, Please pass in the HEX value")
 }
+document.title='Kripu\'s Color Generator'
 // setValue([Values]);
 // console.log(value)
     }
     return (
         <main>
-            <h1> Color <span> Generator</span></h1>
+            <h1> Kripu's Color <span> Generator</span></h1>
             <section>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor='generator'> Color <span> Generator</span></label>
--- color-generator/src/Components/Singlecolor.js
+++ color-generator/src/Components/Singlecolor.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { toast } from 'react-toastify';
 import rgbToHex from './Utils'
 
 const Singlecolor = ({index, weight, rgb, hexColor }) => {
 
-
+const [alert, setAlert] = useState(false);
       const all = [...rgb]
+      //invoking the converting fn
     const hex = rgbToHex(...all)
-    console.log(hex)
+    //check
+    // console.log(hex)
+    
+    //useEffect to clear the repeated toast if we select mulltiple items
+    
+    useEffect(()=>{
+      setTimeout(()=>{
+        setAlert(false)
+      }, 1000)
+    },[alert])
+    //dependecny set to alert
+
+    //toast message to user when alert changes to true
+    if(alert==true){
+      toast.success(`${hex} value copied`)
+    }
     return (
 
 
@@ -14,7 +31,10 @@ const Singlecolor = ({index, weight, rgb, hexColor }) => {
 
 
         <div className='color'>
-          <div  className ='colorBoard'style={{backgroundColor:`rgb(${all})`}} >
+          <div  className ='colorBoard'style={{backgroundColor:`rgb(${all})`}} onClick={()=>{
+            setAlert(true);
+            navigator.clipboard.writeText(hex)
+          }}>
            <div >
            
               <p className={index>5?'color-light' : 'color-dark'}>{weight}%</p>
